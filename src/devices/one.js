d2b7641dcd556ccd506ce6d2b4f4de244a24a481
Jesus Christian <christian.castillo.lozano@gmail.com>
2019-09-17 04:30:41 -0500

    SensationL3d

--- src/devices/one.js
+++ src/devices/one.js
@@ -1,12 +1,12 @@
-var EtherPortClient = require("etherport-client").EtherPortClient;
-var five = require("johnny-five");
+const EtherPortClient = require("etherport-client").EtherPortClient;
+const five = require("johnny-five");
 //const emitter = new EventEmitter();
 
 const { investColor } = require("../helpers/invest_color");
 
-var board = new five.Board({
+const board = new five.Board({
   port: new EtherPortClient({
-    host: "192.168.1.69", // casa 130 lamp -181 nodemcu
+    host: "192.168.1.64", // casa 130 lamp -181 nodemcu
     port: 3030
   }),
   timeout: 1e5
@@ -23,7 +23,7 @@ board.on("ready", function() {
     led: led
   });
 
-  var socket = require("socket.io-client")("http://192.168.1.67:3000"); //pc server
+  const socket = require("socket.io-client")("https://sensationled.herokuapp.com"); //pc server
   socket.on("conectate", data => {
     console.log(data);
   });
