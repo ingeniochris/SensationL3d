1591755b8eefc3d1c6a1c94078fd26e10210d786
Jesus Christian <christian.castillo.lozano@gmail.com>
2019-09-16 20:52:40 -0500

    Sensation led

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ require("./config/passport")(passport);
 
 // middlewares
 app.enable("trust proxy");
-//app.use(express_enforces_ssl());
+app.use(express_enforces_ssl());
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
@@ -60,7 +60,8 @@ app.use(
       `localhost:${app.get("port")}`,
       "sensationled.herokuapp.com/",
       /.*\.sensationled\.herokuapp\.com$/
-    ]
+    ],
+    referers: ['https://sensationled.herokuapp.com'] 
   })
 );
 
