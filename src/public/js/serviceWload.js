d4dbe7b66526ae478333f2764af82c67424a2cec
Jesús Christian Castillo <christian.castillo.lozano@gmail.com>
2020-04-15 02:54:50 -0500

    update modules, use handlebars@4.5.0 and express-handlebars@3.1.0

--- src/public/js/serviceWload.js
+++ src/public/js/serviceWload.js
@@ -3,7 +3,7 @@
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', async () => {
           const reg = await navigator.serviceWorker.register('/service-worker.js')
-                console.log('Service worker registered.', reg);
+               // console.log('Service worker registered.', reg);
         });
       };      
 })();
